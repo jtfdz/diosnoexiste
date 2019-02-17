adf062e21eba06a1edb52d13b84ce8a504bbbff5
Unknown <andresmarmolm@hotmail.com>
2019-02-17 19:26:17 -0400

    Bootstrap incluido y pagina de registro

---
+++ src/main/webapp/js/register.js
@@ -0,0 +1,16 @@
+function postRegister(){
+    let data = {};
+    document.querySelectorAll(".form-control").forEach(i =>{
+        data[i.name] = i.value;
+    });
+
+    var sex = document.getElementsByName('sex');
+    data.sex = sex[0].checked ? "male" : "female";
+    
+    console.log(data);
+}
+    
+    
+
+
+document.getElementById("submits").addEventListener("click", postRegister);
