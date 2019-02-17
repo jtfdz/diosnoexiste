807a0b3d89833570c90fa2e13392cfb6fd700074
Unknown <andresmarmolm@hotmail.com>
2019-02-17 16:29:28 -0400

    Servlets base y propertiesreader

---
+++ src/main/java/com/tfmm/filters/LoggedFilter.java
@@ -0,0 +1,28 @@
+package com.tfmm.filters;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "LoggedFilter", urlPatterns = {"/login", "/register"})
+public class LoggedFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/main/java/com/tfmm/servlets/LogInServlet.java
@@ -0,0 +1,15 @@
+package com.tfmm.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/login", name = "LoginServlet")
+public class LogInServlet extends HttpServlet {
+
+
+
+}
